7791eb7f7093444ff1b9f7de51b4804f30174133
Lucas Wiley <22181786+lucaswiley@users.noreply.github.com>
2025-02-05 17:14:18 -0800

    build errors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,9 +29,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} font-sans bg-background text-foreground antialiased`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
